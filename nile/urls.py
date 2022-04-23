0186912a99f7bc0d213117d799a63503d020b554
ndmichael <ukejemicheal@gmail.com>
2022-04-23 02:52:34 +0100

    added smart_selects to url

--- nile/urls.py
+++ nile/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -22,6 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('fees.urls')),
     path('accounts/', include('allauth.urls')),
+    # path('chaining/', include('smart_selects.urls')),
+    re_path(r'^chaining/', include('smart_selects.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
