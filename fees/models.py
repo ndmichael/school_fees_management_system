e2320493ed2cbefd04ad7d6bac3a25bb87723fd2
ndmichael <ukejemicheal@gmail.com>
2022-04-26 14:46:39 +0100

    updated model, added user image in the data model

--- fees/models.py
+++ fees/models.py
@@ -39,11 +39,19 @@ class StudentProfile (models.Model):
         auto_choose = True,
         sort = True
     )
+    image = models.ImageField(default='avatar.png', upload_to='profile_pics/', blank=True, null=True)
     address  = models.TextField(max_length=1000)
     gender = models.CharField(choices = choices, max_length = 10)
     mobile_number = models.CharField(blank= True, max_length=15)
     admission_date = models.DateField(default=timezone.now)
     record_date  = models.DateTimeField(default=timezone.now)
+
+    @property
+    def photo_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+        else:
+            return "/media/avatar.png"
 
     def __str__(self):
         return f'{self.user.username}'
@@ -54,13 +62,20 @@ class StaffProfile(models.Model):
         ("admin", "ADMIN"),
     )
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-    
+    image = models.ImageField(default='avatar.png', upload_to='profile_pics/', blank=True, null=True)
     address = models.TextField()
     mobileNo = models.CharField(blank= True, max_length = 15)
     DOB = models.DateField()
     gender = models.CharField(choices = choices, max_length = 20)
     department = models.CharField(choices =DEPARTMENT, default="admin", max_length=20)
 
+    @property
+    def photo_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+        else:
+            return "/media/avatar.png"
+            
     def __str__(self):
             return f'{self.user.username}'
 
