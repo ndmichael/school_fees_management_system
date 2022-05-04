e1521bdaf91c7ac410ce149e8bcc3cb3c246f23f
ndmichael <ukejemicheal@gmail.com>
2022-05-04 05:23:09 +0100

    updated, change the username to staff id

--- fees/forms.py
+++ fees/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm , UsernameField
 from .models import StaffProfile, StudentProfile
 
 class UserForm(UserCreationForm):
     email = forms.EmailField()
     username = forms.CharField()
-    username.help_text = ''
+    username = UsernameField(
+        label='User ID',
+        widget=forms.TextInput(attrs={'autofocus': True})
+    )
     class Meta:
         model = User
         fields = ['username','email', 'first_name', 'last_name', 'password1', 'password2']
