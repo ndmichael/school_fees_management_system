521577337dae6ee2832d3ae71176584e813cfbe5
ndmichael <ukejemicheal@gmail.com>
2022-05-22 00:41:14 +0100

    added the delete url

--- fees/urls.py
+++ fees/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, addUser, student, update_student, payment
+from .views import index, addUser, student, update_student, payment, update_payment, delete_payment
 
 urlpatterns = [
     path('', index, name="index"),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('students/all/', student, name="students"),
     path('students/update/<str:username>', update_student, name="update_student"),
     path('payment/', payment, name="make_payment"),
+    path('payment/update/<int:pk>', update_payment, name="update_payment"),
+    path('payment/delete/', delete_payment, name="delete_payment"),
 ]
