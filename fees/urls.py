e754e3c2595c4256c68584260f4d8589f847787f
ndmichael <ukejemicheal@gmail.com>
2022-05-24 13:00:34 +0100

    added url for payment reports

--- fees/urls.py
+++ fees/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import index, addStudent, student, update_student, payment, update_payment, delete_payment, admin_dashboard
+from .views import (
+    index, addStudent, student, update_student, 
+    payment, update_payment, delete_payment, admin_dashboard,
+    payment_report
+)
 
 urlpatterns = [
     path('', index, name="index"),
@@ -9,5 +13,6 @@ urlpatterns = [
     path('students/update/<str:username>', update_student, name="update_student"),
     path('payment/', payment, name="make_payment"),
     path('payment/update/<int:pk>', update_payment, name="update_payment"),
+    path('payment/report/',payment_report, name="payment_report"),
     path('payment/delete/', delete_payment, name="delete_payment"),
 ]
