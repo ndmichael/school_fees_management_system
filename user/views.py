67dae6ea45e2ddcd490390d524adae270b4de4ed
ndmichael <ukejemicheal@gmail.com>
2022-05-28 10:17:13 +0100

    added remark form for student to fill in and commit

--- user/views.py
+++ user/views.py
@@ -8,20 +8,22 @@ from django.contrib import messages
 
 def student_profile(request, username):
     if request.POST:
-        r_form = RemarkForm(request.POST, instance=request.user)
+        r_form = RemarkForm(request.POST)
         if r_form.is_valid():
-            r_form.save(commit=False)
-            r_form.user = request.user
+            form = r_form.save(commit=False)
+            form.student = request.user.student_user
             r_form.save()
             messages.success(request, f"Message Has Been Sent.")
             return redirect(
                 "stud_profile", username=request.user.username
             ) 
+    else:
+        r_form = RemarkForm()
 
     student = Student.objects.filter(user__username=username).first()
     payments = Payment.objects.filter(student=student)
 
-    r_form = RemarkForm()
+    
     context = {
         'student': student,
         'payments': payments,
