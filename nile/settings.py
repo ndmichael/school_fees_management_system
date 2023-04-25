334dc3779173d49807733e1afcad46b486a63d28
ndmichael <ukejemicheal@gmail.com>
2023-04-25 22:40:52 +0100

    changes on the settings

--- nile/settings.py
+++ nile/settings.py
@@ -101,18 +101,18 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
 
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('NAME'),
-        'USER': env('USER') ,
-        'PASSWORD': env('PASSWORD') ,
-        'HOST': 'localhost',
-        'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': env('NAME'),
+        # 'USER': env('USER') ,
+        # 'PASSWORD': env('PASSWORD') ,
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
 
-        # 'NAME': env('PGDATABASE'),
-        # 'USER': env('PGUSER') ,
-        # 'PASSWORD': env('PGPASSWORD') ,
-        # 'HOST': env('PGHOST'),
-        # 'PORT': env('PGPORT'),
+        'NAME': env('PGDATABASE'),
+        'USER': env('PGUSER') ,
+        'PASSWORD': env('PGPASSWORD') ,
+        'HOST': env('PGHOST'),
+        'PORT': env('PGPORT'),
     }
 }
 
