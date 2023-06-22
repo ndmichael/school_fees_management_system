6daaac47a0b65268dab1d43a6181f69857edd80c
ndmichael <ukejemicheal@gmail.com>
2023-06-22 13:53:29 +0100

    updated and add the user url

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import stud_profile, student_profile
+from .views import stud_profile, student_profile, staff_profile
 
 
 urlpatterns = [
     path('student/<str:username>/profile', stud_profile, name="stud_profile"),
+    path('staff/<str:username>/profile', staff_profile, name="staff_profile"),
     path('student/<str:username>/', student_profile, name="student_profile"),
 ]
