62ea47cff9524e358ef2c62cca13b7a6f0e6bd00
ndmichael <ukejemicheal@gmail.com>
2024-08-14 15:31:33 +0100

    filter for the filter package

--- fees/filters.py
+++ fees/filters.py
@@ -1,22 +1,73 @@
 from django_filters import CharFilter, FilterSet
-from fees.models import Student, Payment
+from fees.models import Complaint
 from django import forms
+from crispy_forms.layout import Layout, Submit, Row, Fieldset, Field
+from crispy_forms.helper import FormHelper
+from crispy_bootstrap5.bootstrap5 import FloatingField
+from .forms import ComplaintFilterForm
 
 
-class StudentFilter(FilterSet):
+class ComplaintFilter(FilterSet):
     """Class to filter the Student Model by course or by ID(username)"""
-    # user =  CharFilter(
-    #     label='', 
-    #     field_name='user',  
-    #     lookup_expr='icontains',
-    #     widget=forms.TextInput(
-    # attrs={
-    #         "class":"form-control form-control-lg",
-    #         "placeholder": "student id",
-    #         "max_length":"100"
-    #         }
-    #     )
-    # )
+    
+    
     class Meta:
-        model = Student
-        fields = ['courses', 'us']
+        STATUS  =( ('P', 'Pending'),
+            ('R', 'Resolved'),
+            ('C', 'Closed')
+        )
+        model = Complaint
+        status = forms.ChoiceField(choices=STATUS)
+        fields = ['reference_id', 'status']
+        fields ={
+           'reference_id': ['icontains'] ,
+           'status': [''],
+        } 
+        # form = ComplaintFilterForm
+
+    @property
+    def form(self):
+        form = super().form
+
+        layout_components = list(Field(k, wrapper_class='form-control-sm col-md-3') for k in form.fields.keys()) + [
+            
+        ]
+        helper = FormHelper()
+        helper.form_method = "GET"
+        helper.layout = Layout(
+            Row(
+                *layout_components,
+                Submit('submit', 'filter',  css_class="col-md-3 btn-danger"), 
+            ),   
+            
+
+        )
+
+        form.helper = helper
+
+        return form
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     # Your code
+    #     self.helper = FormHelper()
+    #     self.helper.layout = Layout(
+    #         Row(
+    #             Field("reference_id", wrapper_class='col-md-6', css_class="row-fluid"),
+    #             FloatingField("status", wrapper_class='col-md-6', css_class="row-fluid"),
+    #         ),  
+    #         Submit('submit', 'Create Account', css_class="col-12 btn-lg btn-danger fw-bold")             
+    #     )
+
+    # def __init__(self, *args, **kwargs):
+    #     # super(ComplaintFilter, self).__init__(*args, **kwargs)
+
+    #     super().__init__(*args, **kwargs)
+    #     self.helper = FormHelper()
+    #     self.helper.layout = Layout(
+    #         Row(
+    #             FloatingField("reference_id", wrapper_class='col-md-6', css_class="row-fluid"),
+    #             # FloatingField("status", wrapper_class='col-md-6', css_class="row-fluid"),
+    #         ),  
+    #         Submit('submit', 'Create Account', css_class="col-12 btn-lg btn-danger fw-bold")             
+    #     )
