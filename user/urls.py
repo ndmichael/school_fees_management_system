b20174e56f1a903af95bdfed92b5f2dafb5c25f3
ndmichael <ukejemicheal@gmail.com>
2024-08-22 07:32:13 +0100

    removed one of the student url whicch is stud_profile

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import (
-    stud_profile, 
     student_profile, 
     staff_profile, 
     current_user_profile
@@ -8,8 +7,7 @@ from .views import (
 
 
 urlpatterns = [
-    path('student/<str:username>/profile', stud_profile, name="stud_profile"),
-    path('staff/<str:username>/profile', staff_profile, name="staff_profile"),
-    path('student/<str:username>/', student_profile, name="student_profile"),
+    path('staff/<str:username>', staff_profile, name="staff_profile"),
+    path('student/<str:username>', student_profile, name="student_profile"),
     path('', current_user_profile, name="currentprofile"),
 ]
