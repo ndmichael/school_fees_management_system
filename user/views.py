3af27075d721cccef35c0200a9cc2c53af77d1e3
ndmichael <ukejemicheal@gmail.com>
2024-08-22 07:31:37 +0100

    redirect user to student_profile rather than stud_profile

--- user/views.py
+++ user/views.py
@@ -7,34 +7,34 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
-def stud_profile(request, username):
-    '''*** Student profile section 1 ***
-        === All logics goes her ===
-    '''
+# def stud_profile(request, username):
+#     '''*** Student profile section 1 ***
+#         === All logics goes her ===
+#     '''
     
-    if request.POST:
-        r_form = ComplaintForm(request.POST)
-        if r_form.is_valid():
-            form = r_form.save(commit=False)
-            form.student = request.user.student_user
-            r_form.save()
-            messages.success(request, f"Message Has Been Sent.")
-            return redirect(
-                "stud_profile", username=request.user.username
-            ) 
-    else:
-        r_form = ComplaintForm()
+#     if request.POST:
+#         r_form = ComplaintForm(request.POST)
+#         if r_form.is_valid():
+#             form = r_form.save(commit=False)
+#             form.student = request.user.student_user
+#             r_form.save()
+#             messages.success(request, f"Message Has Been Sent.")
+#             return redirect(
+#                 "stud_profile", username=request.user.username
+#             ) 
+#     else:
+#         r_form = ComplaintForm()
 
-    student = Student.objects.filter(user__username=username).first()
-    payments = Payment.objects.filter(student=student)
+#     student = Student.objects.filter(user__username=username).first()
+#     payments = Payment.objects.filter(student=student)
 
-    context = {
-        'student': student,
-        'payments': payments,
-        'r_form': r_form,
-        "title": "profile page"
-    }
-    return render(request, 'account/stud_profile.html', context)
+#     context = {
+#         'student': student,
+#         'payments': payments,
+#         'r_form': r_form,
+#         "title": "profile page"
+#     }
+#     return render(request, 'account/stud_profile.html', context)
 
 
 @login_required
@@ -72,4 +72,4 @@ def current_user_profile(request):
 
     if request.user.is_staff:
         return redirect('admin_dashboard')
-    return redirect('clientprofile', username=request.user.username)
+    return redirect('student_profile', username=request.user.username)
