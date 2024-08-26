00169484713c65738ae10076ea20c869ce40f09e
ndmichael <ukejemicheal@gmail.com>
2024-08-26 17:33:58 +0100

    added the url for the user_settings

--- user/urls.py
+++ user/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import (
     student_profile, 
     staff_profile, 
-    current_user_profile
+    current_user_profile,
+    user_settings
 )
 
 
 urlpatterns = [
     path('staff/<str:username>', staff_profile, name="staff_profile"),
     path('student/<str:username>', student_profile, name="student_profile"),
+    path('users/settings', user_settings, name='user_settings'),
     path('', current_user_profile, name="currentprofile"),
 ]
