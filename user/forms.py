b051a343d4e91af944bbbcd929b743adc3fe506d
ndmichael <ukejemicheal@gmail.com>
2024-08-26 12:47:41 +0100

    Add user and staff forms

--- user/forms.py
+++ user/forms.py
@@ -1,4 +1,7 @@
 from allauth.account.forms import SignupForm, LoginForm
+from django import forms
+from django.contrib.auth.models import User
+from fees.models import Staff
 
 class SelfLoginForm (LoginForm):
 
@@ -8,3 +11,17 @@ class SelfLoginForm (LoginForm):
             {'class': 'form-control-lg rounded-4  mb-4'})
         self.fields["password"].widget.attrs.update(
             {'class': 'form-control-lg rounded-4 mb-4 '})
+        
+
+class UserUpdateForm(forms.ModelForm):
+    """Form to update staff as user."""
+    class Meta:
+        model = User
+        fields = ['last_name', 'first_name', 'username', 'email']
+
+
+class StudentUpdateForm(forms.ModelForm):
+    """Form to update staff profile."""
+    class Meta:
+        model = Staff
+        fields = ['image', 'mobileNo', 'dob', 'Address']
