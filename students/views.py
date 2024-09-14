1dd235edc1280e9ab1d1aa62cc651c7240e138c1
ndmichael <ukejemicheal@gmail.com>
2024-09-14 12:21:09 +0100

    Logic to return the template

--- students/views.py
+++ students/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from fees.models import Student, Payment, Staff, Course
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+@login_required
+def payment_proof(request):
+    '''Manage Payment proof here'''
+    context = {}
+    return render(request, 'students/payment_proof.html',context)
