af05b8cc087294095ee3f3060626dd74dd4a2b1a
ndmichael <ukejemicheal@gmail.com>
2024-09-16 17:03:23 +0100

    Logic to handle payment proof form

--- students/views.py
+++ students/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from fees.models import Student, Payment, Staff, Course
 from django.contrib import messages
@@ -11,6 +11,36 @@ from .forms import PaymentProofForm
 @login_required
 def payment_proof(request):
     '''Manage Payment proof here'''
+
+    if request.POST:
+        form = PaymentProofForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            pp_form = form.save(commit=False)
+
+            student = get_object_or_404(
+                Student,
+                user = request.user
+            ) 
+
+            if student:
+                pp_form.student = student
+                pp_form.save()
+                messages.success(
+                    request, f"Payment proof has been submitted"
+                )
+            else:
+                messages.danger(
+                    request, f"User doesn't exist"
+                )
+
+            
+        return redirect(
+                    "payment_proof"
+                )   
+             
+            
+        
     form = PaymentProofForm
     context = {
         'pp_form': form
