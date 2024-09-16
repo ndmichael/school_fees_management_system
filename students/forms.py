7f78e4cd4b215c3280995d25053b4b204c9fa5a9
ndmichael <ukejemicheal@gmail.com>
2024-09-16 14:46:07 +0100

    Styled the form

--- students/forms.py
+++ students/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from fees.models import Student, Payment, Complaint
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Row, Div
+from crispy_bootstrap5.bootstrap5 import FloatingField
 
 
 class PaymentProofForm(forms.ModelForm):
@@ -8,3 +11,23 @@ class PaymentProofForm(forms.ModelForm):
     class Meta:
         model = Payment
         fields = ['academic_year', 'semester', 'amount', 'payment_method', 'image']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.disable_csrf = True
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Row(
+                FloatingField("academic_year", wrapper_class='col-md-6'),
+                FloatingField("semester", wrapper_class='col-md-6'),
+                FloatingField("amount", wrapper_class='col-md-6'),
+                FloatingField("payment_method", wrapper_class='col-md-6'),
+                FloatingField("image", wrapper_class='col-md-12'),
+                Div(
+                    Submit('submit', 'SEND PROOF', css_class='btn-lg col-12 px-2')
+                )
+                
+            ),
+
+        )
