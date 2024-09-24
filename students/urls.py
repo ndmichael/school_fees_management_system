f97d69a02ff49cd589f2b923e1706f7dce9dd022
ndmichael <ukejemicheal@gmail.com>
2024-09-24 15:17:37 +0100

    url for the payment complaints page

--- students/urls.py
+++ students/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import (
     payment_proof,
     make_payment,
+    payment_complaints,
 )
 
 
 urlpatterns = [
-    path('payment/proof', payment_proof, name="payment_proof"),
-    path('make/payment/', make_payment, name="make_payment")
+    path('payment/proof/', payment_proof, name="payment_proof"),
+    path('make/payment/', make_payment, name="make_payment"),
+    path('payment/complaints/', payment_complaints, name="payment_complaints")
 ]
