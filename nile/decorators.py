f14c45fbb40fece3a50b126dbb3215a7d1c9b254
ndmichael <ukejemicheal@gmail.com>
2024-09-25 23:21:00 +0100

    student_required decorator added

--- nile/decorators.py
+++ nile/decorators.py
@@ -7,6 +7,16 @@ def staff_required(view_func):
     def wrapper(request, *args, **kwargs):
         if not request.user.is_staff:
             messages.error(request, "You do not have permission to access this page.")
-            return redirect("/")
+            return redirect("account_login")
+        return view_func(request, *args, **kwargs)
+    return wrapper
+
+
+def student_required(view_func):
+    @login_required(login_url='account_login')  # Redirect to login if not logged in
+    def wrapper(request, *args, **kwargs):
+        if not hasattr(request.user, 'student_user'):
+            messages.error(request, "You do not exist as a student.")
+            return redirect("account_login")
         return view_func(request, *args, **kwargs)
     return wrapper
