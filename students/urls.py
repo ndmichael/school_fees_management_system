c0f4db6dcdb83cac660f888552663f5a30689a50
ndmichael <ukejemicheal@gmail.com>
2024-09-26 12:59:40 +0100

    url for dashboard

--- students/urls.py
+++ students/urls.py
@@ -3,10 +3,12 @@ from .views import (
     payment_proof,
     make_payment,
     payment_complaints,
+    student_dashboard
 )
 
 
 urlpatterns = [
+    path('student-dashboard/', student_dashboard, name="student_dashboard"),
     path('payment/proof/', payment_proof, name="payment_proof"),
     path('make/payment/', make_payment, name="make_payment"),
     path('payment/complaints/', payment_complaints, name="payment_complaints")
