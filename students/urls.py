d84847af5e8cac38c509b7ce2d62723219284078
ndmichael <ukejemicheal@gmail.com>
2024-09-28 14:27:29 +0100

    student profile added

--- students/urls.py
+++ students/urls.py
@@ -3,13 +3,15 @@ from .views import (
     payment_proof,
     make_payment,
     payment_complaints,
-    student_dashboard
+    student_dashboard,
+    student_profile,
 )
 
 
 urlpatterns = [
-    path('student-dashboard/', student_dashboard, name="student_dashboard"),
+    path('dashboard/', student_dashboard, name="student_dashboard"),
     path('payment/proof/', payment_proof, name="payment_proof"),
     path('make/payment/', make_payment, name="make_payment"),
-    path('payment/complaints/', payment_complaints, name="payment_complaints")
+    path('payment/complaints/', payment_complaints, name="payment_complaints"),
+    path('<str:username>', student_profile, name="student_profile"),
 ]
