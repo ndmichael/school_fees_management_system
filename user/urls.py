270b76f2124e123b3127ec5f0ad73d1b38b3c79f
ndmichael <ukejemicheal@gmail.com>
2024-09-30 13:14:33 +0100

    URL adjusted

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import (
-    student_profile, 
     staff_profile, 
     current_user_dashboard,
     user_settings
@@ -9,7 +8,6 @@ from .views import (
 
 urlpatterns = [
     path('staff/<str:username>', staff_profile, name="staff_profile"),
-    path('student/<str:username>', student_profile, name="student_profile"),
     path('users/settings', user_settings, name='user_settings'),
     path('', current_user_dashboard, name="currentdashboard"),
 ]
